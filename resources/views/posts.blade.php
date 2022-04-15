199844faa6bc9f761f0128acb11b476da600e63a
ghassan bounni <ghassan.bounni18@gmail.com>
2022-04-15 23:45:26 +0300

    add posts page and setup the routes

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/app.css" />
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/first-post">First Post</a></h1>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus neque, quibusdam unde nulla consequatur, deserunt voluptas illo beatae dolores quo delectus illum minima eum aspernatur quia, molestiae ipsum atque iusto.
+        Mollitia molestias minus, labore sapiente rerum distinctio? Temporibus alias delectus perspiciatis, enim qui quas dolorum aliquam amet officiis dolore incidunt cumque doloremque magnam voluptate, voluptatem doloribus reiciendis illum ipsa odit.
+        Inventore ratione iure repellendus molestias, doloremque corrupti ipsa nihil, voluptates, accusantium totam perferendis dolor voluptate ipsum asperiores mollitia perspiciatis ducimus sunt quod aliquid consequatur deleniti iste ab. Officiis, dicta laborum?
+        Eaque aliquid ab, temporibus a quisquam tenetur reiciendis veniam tempora quibusdam, recusandae dolore error laborum eligendi veritatis illum minus quas fugiat reprehenderit illo accusamus cupiditate iure fugit explicabo aspernatur? Esse!
+        Esse labore doloribus quaerat repudiandae voluptate aliquid. Aliquam accusamus quod qui officia quasi nihil. Impedit dolor quibusdam, magni aliquam minus sint quas eius officia voluptatibus ipsa? Obcaecati sunt saepe vero.</p>
+    </article>
+    <article>
+        <h1><a href="/posts/second-post">Second Post</a></h1>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus neque, quibusdam unde nulla consequatur, deserunt voluptas illo beatae dolores quo delectus illum minima eum aspernatur quia, molestiae ipsum atque iusto.
+        Mollitia molestias minus, labore sapiente rerum distinctio? Temporibus alias delectus perspiciatis, enim qui quas dolorum aliquam amet officiis dolore incidunt cumque doloremque magnam voluptate, voluptatem doloribus reiciendis illum ipsa odit.
+        Inventore ratione iure repellendus molestias, doloremque corrupti ipsa nihil, voluptates, accusantium totam perferendis dolor voluptate ipsum asperiores mollitia perspiciatis ducimus sunt quod aliquid consequatur deleniti iste ab. Officiis, dicta laborum?
+        Eaque aliquid ab, temporibus a quisquam tenetur reiciendis veniam tempora quibusdam, recusandae dolore error laborum eligendi veritatis illum minus quas fugiat reprehenderit illo accusamus cupiditate iure fugit explicabo aspernatur? Esse!
+        Esse labore doloribus quaerat repudiandae voluptate aliquid. Aliquam accusamus quod qui officia quasi nihil. Impedit dolor quibusdam, magni aliquam minus sint quas eius officia voluptatibus ipsa? Obcaecati sunt saepe vero.</p>
+    </article>
+    <article>
+        <h1><a href="/posts/third-post">third Post</a></h1>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus neque, quibusdam unde nulla consequatur, deserunt voluptas illo beatae dolores quo delectus illum minima eum aspernatur quia, molestiae ipsum atque iusto.
+        Mollitia molestias minus, labore sapiente rerum distinctio? Temporibus alias delectus perspiciatis, enim qui quas dolorum aliquam amet officiis dolore incidunt cumque doloremque magnam voluptate, voluptatem doloribus reiciendis illum ipsa odit.
+        Inventore ratione iure repellendus molestias, doloremque corrupti ipsa nihil, voluptates, accusantium totam perferendis dolor voluptate ipsum asperiores mollitia perspiciatis ducimus sunt quod aliquid consequatur deleniti iste ab. Officiis, dicta laborum?
+        Eaque aliquid ab, temporibus a quisquam tenetur reiciendis veniam tempora quibusdam, recusandae dolore error laborum eligendi veritatis illum minus quas fugiat reprehenderit illo accusamus cupiditate iure fugit explicabo aspernatur? Esse!
+        Esse labore doloribus quaerat repudiandae voluptate aliquid. Aliquam accusamus quod qui officia quasi nihil. Impedit dolor quibusdam, magni aliquam minus sint quas eius officia voluptatibus ipsa? Obcaecati sunt saepe vero.</p>
+    </article>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/posts', function () {
+    return view('posts');
+});
+Route::get('/posts/{post}', function ($slug) {
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+    if (!file_exists($path)) {
+        // ddd("file does not exist");
+        // ddd($path);
+        // abort(404);
+        // return view('posts');
+        return redirect('/posts');
+    }
+
+    // $post = file_get_contents($path);
+
+    $post = cache()->remember("posts.{$slug}", 5, fn () => file_get_contents($path));
+
+
+    return view('post', ['post' => $post]);
+})->where('post', '[a-z0-9_\-]+');
