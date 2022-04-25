84e2f17649e9ad8f2e49f6605bf9fcc96e873d42
ghassan bounni <ghassan.bounni18@gmail.com>
2022-04-25 23:39:46 +0300

    finish category page and solve N+1 problem

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 
@@ -19,12 +20,18 @@ Route::get('/', function () {
 });
 Route::get('/posts', function () {
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Post::with('category')->get()
     ]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', [
         'post' => $post
+    ]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 });
